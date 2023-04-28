7a648952207c8cfbeead6ce572cfb8207c4f5e76
egor mikhajlov <ickzn@ya.ru>
2023-04-28 12:45:00 +0300

    Добавил авторизацию на сайте

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask
+
 from manage import bp
-from core import db
+from core import db, login_manager
 from core import csrf
 from models import *
-
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config')
     db.init_app(app)
     csrf.init_app(app)
+    login_manager.init_app(app)
     app.register_blueprint(bp)
     return app
