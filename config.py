81d203cfd5f6b4ad59630cc304f059c516d8119d
egor mikhajlov <ickzn@ya.ru>
2023-07-03 14:22:09 +0300

    edit config

--- config.py
+++ config.py
@@ -6,5 +6,3 @@ SECRET_KEY = '8e+p,Dxn<lVJ>%c@"N/#~DhiQ]BD=*@x\\(xU^c!El@E0d,1<)_(bbj3fGc&`5I&[r
 # Параметры авторизации
 REMEMBER_COOKIE_DURATION = 1
 
-# Для телеграмм бота
-TELEGA_TOKEN = '6047085082:AAGnMcr-dPRdg-bGyB4jQCcsPmQI_1DdG_o'
